55cbc17f5f3d1a4c7680d6e5c206c9d34e6f6b76
NastyNastya2024 <166286695+NastyNastya2024@users.noreply.github.com>
2024-05-18 18:57:46 +0300

    add new const

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pygame
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption('Pygame Game')
+icon = pygame.image.load('images/icon.png')
+
 running = True
 while running:
     pass
+
+
 
 pygame.quit()
